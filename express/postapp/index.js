d8f932d80858a727cef3ec9dc2d198c1d112d597
GabrielAlvesBM <gabrielalves6002@gmail.com>
2024-11-08 16:52:11 -0300

    feat: create Post

--- express/postapp/index.js
+++ express/postapp/index.js
@@ -3,22 +3,34 @@ const app = express();
 const handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
 const bodyParser = require('body-parser');
 
+const Post = require('./models/Post');
+
 // Config
     // Template Engine
         app.engine('handlebars', handlebars.engine);
         app.set('view engine', 'handlebars');
 
     // Body Parser
-        // app.use(bodyParser.urlencoded({ extend: false }));
+        app.use(bodyParser.urlencoded({ extend: false }));
         app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 app.get('/cad', (req, res) => {
     res.render('formulario');
 });
 
 app.post('/add', (req, res) => {
-
-    res.send(`Titulo: ${ req.body.titulo } Conteúdo: ${ req.body.conteudo }` + 'Formulário Recebido!');
+    Post.create({
+        titulo: req.body.titulo,
+        conteudo: req.body.conteudo
+    }).then(() => {
+        res.redirect('/');
+    }).catch((error) => {
+        res.send('Houve um erro: ' + error);
+    });
 });
 
 app.listen(8000, () => {
