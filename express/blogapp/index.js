e91f7fac293e36e86568df0ce52a196f020ca2f8
Gabriel Alves <gabrielalves6002@gmail.com>
2024-11-12 17:07:35 -0300

    feat: add bootstrap

--- express/blogapp/index.js
+++ express/blogapp/index.js
@@ -2,15 +2,20 @@ const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
+const path = require('path');
 // const mongoose = require('mongoose');
 
 const admin = require('./routes/admin');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+// Rotas
 
 app.use('/admin', admin);
 
