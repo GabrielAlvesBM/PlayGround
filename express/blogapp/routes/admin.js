08c1992374e3c0a3c528793b4061b86573043068
Gabriel Alves <gabrielalves6002@gmail.com>
2024-11-13 12:06:28 -0300

    feat: add Categories in db

--- express/blogapp/routes/admin.js
+++ express/blogapp/routes/admin.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+require('../models/Category');
+const Category = mongoose.model('categories')
 
 router.get('/', (req, res) => {
     res.render('admin/index');
@@ -15,6 +18,21 @@ router.get('/categories', (req, res) => {
 
 router.get('/categories/add', (req, res) => {
     res.render('admin/addcategories');
+});
+
+router.post('/categories/new', (req, res) => {
+    const newCategorie = {
+        name: req.body.name,
+        slug: req.body.slug
+    };
+
+    new Category(newCategorie).save().then(() => {
+        console.log('Categoria salva com sucesso!');
+        res.send('Categoria salva com sucesso!');
+    }).catch((error) => {
+        console.error('Erro ao cadastrar categoria: ' + error);
+        res.send('Erro ao cadastrar categoria: ' + error);
+    })
 });
 
 module.exports = router;
