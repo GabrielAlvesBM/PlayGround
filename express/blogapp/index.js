e45b0767ad113016de4b273577418a8c4d4b3697
Gabriel Alves <gabrielalves6002@gmail.com>
2024-11-14 15:18:11 -0300

    feat: checking categories and alert messages

--- express/blogapp/index.js
+++ express/blogapp/index.js
@@ -4,22 +4,38 @@ const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 const admin = require('./routes/admin');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'Secret Secreta porra!',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+})
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost/blogapp').then(() => {
     console.log('Mongodb Conectado!');
 }).catch((error) => {
     console.error('Erro ao conectar no mongodb: ' + error);
 })
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Rotas
 app.use('/admin', admin);
--- express/blogapp/routes/admin.js
+++ express/blogapp/routes/admin.js
@@ -21,18 +21,41 @@ router.get('/categories/add', (req, res) => {
 });
 
 router.post('/categories/new', (req, res) => {
-    const newCategorie = {
-        name: req.body.name,
-        slug: req.body.slug
+    let errors = []
+
+    if (!req.body.name || typeof req.body.name == undefined || req.body.name == null ) {
+        errors.push({ text: 'Nome Invalido' });
     };
 
-    new Category(newCategorie).save().then(() => {
-        console.log('Categoria salva com sucesso!');
-        res.send('Categoria salva com sucesso!');
-    }).catch((error) => {
-        console.error('Erro ao cadastrar categoria: ' + error);
-        res.send('Erro ao cadastrar categoria: ' + error);
-    })
+    if (!req.body.slug || typeof req.body.slug == undefined || req.body.name == null) {
+        errors.push({ text: 'Slug Invalido' });
+    };
+
+    if (req.body.name.length < 2) {
+        errors.push({ text: 'Nome muito curto' });
+    };
+
+    if (req.body.slug.length < 2) {
+        errors.push({ text: 'Slug muito curto' });
+    };
+
+    if (errors.length > 0) {
+        res.render('admin/addcategories', {errors: errors});
+    } else {
+        const newCategorie = {
+            name: req.body.name,
+            slug: req.body.slug
+        };
+    
+        new Category(newCategorie).save().then(() => {
+            console.log('Categoria salva com sucesso!');
+            req.flash('success_msg', 'Categoria criada com sucesso!');
+            res.redirect('/admin/categories');
+        }).catch((error) => {
+            req.flash('error_msg', 'Houve um erro ao salvar a categoria, tente novamente!')
+            res.redirect('/admin');
+        })
+    }
 });
 
 module.exports = router;
