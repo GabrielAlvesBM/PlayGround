4637a2698282a73524c222d8b77f915a7592dd83
Gabriel Alves <gabrielalves6002@gmail.com>
2024-11-26 14:32:00 -0300

    create(vueJS): app (for tests)

---
+++ vueJS/app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  data() { 
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin: 60px;
+}
+</style>
---
+++ vueJS/app/src/components/MainHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="header">
+        Header Porra
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'MainHeader'
+    }
+</script>
+
+<style>
+    .header {
+        background-color: #000;
+        color: #fff;
+    }
+</style>
