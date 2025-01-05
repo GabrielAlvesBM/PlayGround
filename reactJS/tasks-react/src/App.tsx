3611be46efda00961ee8815d6fe6d30b7e80da48
Gabriel Alves <gabrielalves6002@gmail.com>
2025-01-05 15:59:13 -0300

    feat: MVP project

--- reactJS/tasks-react/src/App.tsx
+++ reactJS/tasks-react/src/App.tsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState<string[]>([])
+  const [taskInput, setTaskInput] = useState<string>('')
+
+  function addTask () {
+    if (taskInput.trim()) {
+      setTasks([...tasks, taskInput])
+      setTaskInput('')
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input className='task-input' type="text" name="taskInput" id='taskInput'
+        placeholder='Insira sua tarefa' autoComplete='off' value={taskInput}
+        onChange={(event) => setTaskInput(event.target.value)}
+        onKeyDown={(event) => {
+          if (event.key == 'Enter') addTask()
+        }} />
+      
+      <ul className='task-list'>
+        {tasks.map((task, index) => (
+          <li id={index.toString()} key={index}>{task}</li>
+        ))}
+      </ul>
     </>
   )
 }
